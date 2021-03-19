0f5334c29c60f0b6f2c93328c17ef7158e2b291c
Rob-a-dob-dob <80466262+Rob-a-dob-dob@users.noreply.github.com>
2021-03-19 13:20:03 +0000

    Update dataParse.c

--- dataParse.c
+++ dataParse.c
@@ -12,5 +12,14 @@ struct packet
 int dataParse(int sPort, int dPort, int sMac, int dMac, int target[], int source[], char data[])
 {   
     struct packet PingReq;
+    
+    PingReq.sPort = sPort;
+    PingReq.dPort = dPort;
+    PingReq.sMac = sMac;
+    PingReq.dMac = dMac;
+    PingReq.target = target;
+    PingReq.source = source;
+    strcpy(PingReq.data, data);
+    
     return 0;
 }
