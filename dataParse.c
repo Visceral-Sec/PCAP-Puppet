be899c2bcfeefe24b0ccf724e26b3bf4b5e09ee4
Rob-a-dob-dob <80466262+Rob-a-dob-dob@users.noreply.github.com>
2021-03-19 13:11:11 +0000

    Update dataParse.c

--- dataParse.c
+++ dataParse.c
@@ -7,11 +7,10 @@ struct packet
     int target[];
     int source[];
     char data[];
-};
+}
 
 int dataParse(int sPort, int dPort, int sMac, int dMac, int target[], int source[], char data[])
 {   
     struct packet PingReq;
-    PingReq.sPort
     return 0;
 }
