0151ae182cba4084d48f2642cc2507a73ac24fa5
cafelargo <clavebreve@outlook.com>
2021-03-19 12:09:17 +0000

    adding functions to allow parallel working

--- puppet-backend.c
+++ puppet-backend.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+void constructPacket()
+{
+    return;
+}
+
+void dataParse()
+{
+    return;
+}
+
+void writeToFile()
+{
+    return;
+}
+
 int main()
 {
     puts("Hello World");
