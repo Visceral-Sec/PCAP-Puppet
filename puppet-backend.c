89215b93eaf58b824902a2be3edc39a5deed07a6
cafelargo <clavebreve@outlook.com>
2021-03-19 12:12:37 +0000

    work on these separate files as if they're functions for main

--- puppet-backend.c
+++ puppet-backend.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
-void constructPacket()
-{
-    return;
-}
-
-void dataParse()
-{
-    return;
-}
-
-void writeToFile()
-{
-    return;
-}
-
 int main()
 {
     puts("Hello World");
     return 0;
-    //test
 }
