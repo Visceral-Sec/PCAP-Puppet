82ff05f71be7959ce2b6945264bfc875af2ccee5
Rob-a-dob-dob <80466262+Rob-a-dob-dob@users.noreply.github.com>
2021-03-19 14:09:48 +0000

    Update dataParse.c

--- dataParse.c
+++ dataParse.c
@@ -1,20 +1,34 @@
 struct packet
 {
-    int sPort;
-    int dPort;
-    int sMac[];
-    int dMac[];
-    int target[];
-    int source[];
+    //char sPort;
+    //char dPort;
+    char sMac[6];
+    char dMac[6];
+    char target[4];
+    char source[4];
     char data[];
 };
 
-int dataParse(int sPort, int dPort, int sMac, int dMac, int target[], int source[], char data[])
+int dataParse(/*int sPort, int dPort, */int sMac[], int dMac[], int target[], int source[], char data[])
 {   
     struct packet PingReq;
     
-    PingReq.sPort = sPort;
-    PingReq.dPort = dPort;
+    for(int i = 0; i < sMac.length(); i += 3)
+    {
+        int digit1 = sMac[i] - 48;
+        int digit2 = sMac[i + 1] - 48;
+        if(digit1 > 9)
+        {
+            digit1 -= 39;
+        }
+        if(digit2 > 9)
+        {
+            digit2 -= 39;
+        }
+        
+        PingReq.sMac[i/3] = digit1*16 + digit2
+    }
+    
     PingReq.sMac = sMac;
     PingReq.dMac = dMac;
     PingReq.target = target;
