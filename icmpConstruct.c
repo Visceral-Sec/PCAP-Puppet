adf86d61bc94c99f7a75a2705d4a229ede70005f
Rob-a-dob-dob <80466262+Rob-a-dob-dob@users.noreply.github.com>
2021-03-30 16:18:33 +0100

    Update icmpConstruct.c

--- icmpConstruct.c
+++ icmpConstruct.c
@@ -1,5 +1,10 @@
 int icmpConstruct())
 {
+    strncat(packetOut, 0x08, 1);//icmp ping request
+    strncat(packetOut, 0x00, 1);//code is 0
+    strncat(packetOut, 0x????, 2);//icmp check sum, I'll figure it out later
+    strncat(packetOut, 0x0001, 2);//identifier
+    strncat(packetOut, 0x0004, 2);//sequence number 
     
     return 0;
 }
