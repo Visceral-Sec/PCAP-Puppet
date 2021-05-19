f8deca449c04c7d5f9fc3ad29b9501450611bf22
Rob-a-dob-dob <80466262+Rob-a-dob-dob@users.noreply.github.com>
2021-05-19 18:15:16 +0100

    Update udpConstruct.c

--- udpConstruct.c
+++ udpConstruct.c
@@ -4,7 +4,8 @@ void udpConstruct(char icmpSegment[])
     int l_emptyPointer = 0;
     insertVarInto(PingReq.sPort, udpSegment, l_emptyPointer, 2);
     insertVarInto(PingReq.dPort, udpSegment, l_emptyPointer, 2);
-    insertVarInto(8 + pingReq.data, tcpSegment, l_emptyPointer, 2);
-    insertVarInto(checkSum, tcpSegment, l_emptyPointer, 2);
+    insertVarInto(8 + strlen(PingReq.payload), udpSegment, l_emptyPointer, 2);
+    insertVarInto(checkSum, udpSegment, l_emptyPointer, 2);
+    insertVarInto(PingReq.payload, udpSegment, l_emptyPointer, strlen(PingReq.payload));
     return;
 }
