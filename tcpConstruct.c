d23c2967a1101a2cb89026343abf63ad12a05cc8
Rob-a-dob-dob <80466262+Rob-a-dob-dob@users.noreply.github.com>
2021-05-19 03:12:08 +0100

    Create tcpConstruct.c

---
+++ tcpConstruct.c
@@ -0,0 +1,14 @@
+void tcpConstruct(char icmpSegment[])
+{
+    int l_emptyPointer = 0;
+    insertVarInto(PingReq.sPort, tcpSegment, l_emptyPointer, 2);
+    insertVarInto(PingReq.dPort, tcpSegment, l_emptyPointer, 2);
+    insertVarInto(seqNum, tcpSegment, l_emptyPointer, 4);
+    insertVarInto(AckNum, tcpSegment, l_emptyPointer, 4);
+    tcpSegment[l_emptyPointer++] = flags;
+    insertVarInto(windowSize, tcpSegment, l_emptyPointer, 2);
+    insertVarInto(checkSum, tcpSegment, l_emptyPointer, 2);
+    tcpSegment[l_emptyPointer++] = 0x00; tcpSegment[l_emptyPointer++] = 0x00;//Urgent pointer?
+    //then a bunch of options that are scary
+    return;
+}
